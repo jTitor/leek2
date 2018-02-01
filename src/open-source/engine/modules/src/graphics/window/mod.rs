64dfa43cf302ccad4ec95ff5153e55e9dead70b2
Me <me@me.me>
2018-01-31 23:38:36 -0800

    Starting to move winit event conversion out to window struct.

--- src/open-source/engine/modules/src/graphics/window/mod.rs
+++ src/open-source/engine/modules/src/graphics/window/mod.rs
@@ -3,6 +3,6 @@ pub mod window;
 pub mod errors;
 pub mod internal;
 
-pub use self::types::{EventType, Visibility};
+pub use self::types::{EventType, Visibility, convert_winit_event};
 pub use self::window::Window;
 pub use self::errors::{WindowBuilderError, WindowError};
