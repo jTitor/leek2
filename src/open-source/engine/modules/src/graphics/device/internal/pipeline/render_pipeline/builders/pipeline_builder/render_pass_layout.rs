7b9f0560fe24d30df88f59bb8fa70e608facbc57
jTitor <me@me.me>
2018-03-07 22:48:53 -0800

    Implemented RenderPassLayout.

--- src/open-source/engine/modules/src/graphics/device/internal/pipeline/render_pipeline/builders/pipeline_builder/render_pass_layout.rs
+++ src/open-source/engine/modules/src/graphics/device/internal/pipeline/render_pipeline/builders/pipeline_builder/render_pass_layout.rs
@@ -1,9 +1,21 @@
 /*!
  * Defines the RenderPassLayout struct.
  */
+use gfx_hal::{self as hal, image as i, pass, pso}
 
 /**
  * Defines the structure of a Pass for
  * a PipelineBuilder.
  */
-pub struct RenderPassLayout {}
+#[derive(Debug)]
+pub struct RenderPassLayout {
+	pub attachments: Vec<pass::Attachment>,
+	pub subpass_descriptions: Vec<pass::SubpassDesc>,
+	pub dependencies: Vec<pass::SubpassDependency>
+}
+
+impl RenderPassLayout {
+	fn new() -> RenderPassLayout {
+		RenderPassLayout::default()
+	}
+}
